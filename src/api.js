f472849e290f0b5fb07fd533a2a31d67e99e7b40
Breno Lemos <brenomiros@gmail.com>
2020-08-10 18:36:59 -0300

    Login Criar

--- src/api.js
+++ src/api.js
@@ -19,8 +19,21 @@ export function USER_GET(token) {
     options: {
       method: 'GET',
       headers: {
-        Authorization: 'Bearer ' + token
+        Authorization: 'Bearer ' + token,
       },
+    },
+  };
+}
+
+export function USER_POST(body) {
+  return {
+    url: API_URL + '/api/user',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
     },
   };
 }
@@ -31,7 +44,7 @@ export function TOKEN_VALIDATE_POST(token) {
     options: {
       method: 'POST',
       headers: {
-        Authorization: 'Bearer ' + token
+        Authorization: 'Bearer ' + token,
       },
     },
   };
